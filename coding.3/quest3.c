fc990b6fc67890edc9df05f7fa93237dcd128109
Scoutamass <42789432+Scoutamass@users.noreply.github.com>
2024-09-09 01:46:00 +0000

    Did question 3

--- coding.3/quest3.c
+++ coding.3/quest3.c
@@ -1,1 +1,16 @@
-{\rtf1}
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	int x = fork();
+	if(x == 0)
+	{
+		printf("hello");
+		return 0;
+	}
+	wait(NULL);
+	printf("goodbye");
+}
